ad79dffe6a30564f40c74be56ba96f937ffc0cbe
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-05-15 16:59:13 +0900

    ci: eslint, husky, vitest 셋업 완료

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -7,11 +7,15 @@ module.exports = {
   extends: [
     "@vercel/style-guide/eslint/node",
     "@vercel/style-guide/eslint/browser",
-    "@vercel/style-guide/eslint/typescript",
     "@vercel/style-guide/eslint/react",
     "@vercel/style-guide/eslint/next",
+    "@vercel/style-guide/eslint/typescript",
     "eslint-config-turbo",
   ].map(require.resolve),
+  rules: {
+    "import/no-default-export": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
   globals: {
     React: true,
     JSX: true,
