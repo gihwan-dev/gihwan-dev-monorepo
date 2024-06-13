5574b82ac9ccd59be5d15528e920e399c182f89b
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-06-13 19:37:23 +0900

    feat: 체크박스 공용 컴포넌트 개발

--- apps/code-it-test/src/app/assets/icon/CheckBox.tsx
+++ apps/code-it-test/src/app/assets/icon/CheckBox.tsx
@@ -1,7 +1,7 @@
-"use client";
+'use client';
 
-import {motion} from 'framer-motion';
-import {draw} from "~/src/app/const/framer-variants";
+import { motion } from 'framer-motion';
+import { draw } from '~/src/app/const/framer-variants';
 
 interface CheckBoxProps {
   isChecked: boolean;
@@ -10,36 +10,40 @@ interface CheckBoxProps {
 
 export default function CheckBox({ isChecked, onClick }: CheckBoxProps) {
   return (
-      <button className="w-fit h-fit bg-none border-none" onClick={onClick} type="button"  >
-        <motion.svg
-            animate="visible"
-            fill="none"
-            height="32"
-            initial="hidden"
-            viewBox="0 0 32 32"
-            width="32"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle
-              className={"transition-all duration-100 ease-in-out"}
-              cx="16"
-              cy="16"
-              fill={isChecked ? '#7C3AED' : '#FEFCE8'}
-              r={isChecked ? '16' : '15'}
-              stroke={isChecked ? '' : '#0F172A'}
-              strokeWidth={isChecked ? '' : '2'}
+    <button
+      className="h-fit w-fit border-none bg-none"
+      onClick={onClick}
+      type="button"
+    >
+      <motion.svg
+        animate="visible"
+        fill="none"
+        height="32"
+        initial="hidden"
+        viewBox="0 0 32 32"
+        width="32"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          className={'transition-all duration-100 ease-in-out'}
+          cx="16"
+          cy="16"
+          fill={isChecked ? '#7C3AED' : '#FEFCE8'}
+          r={isChecked ? '16' : '15'}
+          stroke={isChecked ? '' : '#0F172A'}
+          strokeWidth={isChecked ? '' : '2'}
+        />
+        {isChecked ? (
+          <motion.path
+            d="M8 16.2857L13.8182 22L24 12"
+            stroke="#FEFCE8"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="4"
+            variants={draw}
           />
-          {isChecked ? (
-              <motion.path
-                  d="M8 16.2857L13.8182 22L24 12"
-                  stroke="#FEFCE8"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="4"
-                  variants={draw}
-              />
-          ) : null}
-        </motion.svg>
-      </button>
+        ) : null}
+      </motion.svg>
+    </button>
   );
 }
--- apps/code-it-test/src/app/assets/icon/stories/CloseIcon.stories.tsx
+++ apps/code-it-test/src/app/assets/icon/stories/CloseIcon.stories.tsx
@@ -1,4 +1,4 @@
-import type {Meta, StoryObj} from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
 import CloseIcon from '../CloseIcon';
 
 const meta: Meta = {
