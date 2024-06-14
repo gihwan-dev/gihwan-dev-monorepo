58c6f681c46fb795d885f8a95531f7af2a77269d
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-06-14 14:00:26 +0900

    feat: 아이콘 스토리북 추가

--- apps/code-it-test/src/app/assets/icon/stories/CloseIcon.stories.tsx
+++ apps/code-it-test/src/app/assets/icon/stories/CloseIcon.stories.tsx
@@ -1,9 +1,16 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import BackgroundGrayWrapper from '~/src/app/assets/icon/stories/wrapper/BackgroundGrayWrapper';
 import CloseIcon from '../CloseIcon';
 
 const meta: Meta = {
   title: 'CloseIcon',
-  component: CloseIcon,
+  component: () => {
+    return (
+      <BackgroundGrayWrapper>
+        <CloseIcon />
+      </BackgroundGrayWrapper>
+    );
+  },
 };
 
 export default meta;
